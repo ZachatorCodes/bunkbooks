d3ad34ecd46ee361b0a03524b066b7f9187587d9
Zach Schwartz <zachatorcodes@gmail.com>
2024-02-29 18:07:12 -0700

    Add test route and controller method to test cookie and session middleware

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  # Test method to confirm cookie and session middleware functionality
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
 end
